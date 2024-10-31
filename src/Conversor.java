c199bb6de54a0189a4188a3f6234e6db951f3979
MarciaAlura <mcrogick@ig.com.br>
2024-10-31 19:31:34 -0300

    update 02

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,15 +1,44 @@
+import br.com.alura.conversormoedas.modelos.Moeda;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Conversor {
     public static void main(String[] args) {
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
+
+        String moedaOrigem ="EUR";
+        String moedaDestino ="BRL";
+        String site = "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/"+moedaOrigem+"/"+moedaDestino;
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(site)).build();
+
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(response.body());
+
+        String json = response.body();
+
+        Gson gson = new Gson();
+        Moeda moeda = gson.fromJson(json, Moeda.class);
+
+        System.out.println(moeda);
+
 
 
 
--- src/br/com/alura/conversormoedas/modelos/Moeda.java
+++ src/br/com/alura/conversormoedas/modelos/Moeda.java
@@ -1,10 +1,128 @@
 package br.com.alura.conversormoedas.modelos;
 
-public class Moeda {
+import java.util.Date;
+import com.google.gson.annotations.SerializedName;
+
+public class Moeda implements Comparable<Moeda> {
+
+    /*
+    "result": "success",
+	"documentation": "https://www.exchangerate-api.com/docs",
+	"terms_of_use": "https://www.exchangerate-api.com/terms",
+	"time_last_update_unix": 1585267200,
+	"time_last_update_utc": "Fri, 27 Mar 2020 00:00:00 +0000",
+	"time_next_update_unix": 1585270800,
+	"time_next_update_utc": "Sat, 28 Mar 2020 01:00:00 +0000",
+	"base_code": "EUR",
+	"target_code": "GBP",
+	"conversion_rate": 0.8412
+	*/
+
+        @SerializedName("result")
+        private String result;
+
+        @SerializedName("documentation")
+        private String documentation;
+
+        @SerializedName("terms_of_use")
+        private String terms_of_use;
+
+        @SerializedName("time_last_update_unix")
+        private int time_last_update_unix;
+
+        @SerializedName("time_last_update_utc")
+        private Date time_last_update_utc;
+
+        @SerializedName("time_next_update_unix")
+        private int time_next_update_unix;
+
+        @SerializedName("time_next_update_utc")
+        private Date time_next_update_utc;
+
+        @SerializedName("base_code")
+        private String base_code;
+
+        @SerializedName("target_code")
+        private String target_code;
+
+        @SerializedName("conversion_rate")
+        private double conversion_rate;
 
         private double valor;
         private String nome;
 
+
+        public double getValor() {
+                return valor;
+        }
+
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+
+        public String getBase_code() {
+                return base_code;
+        }
+
+        public String getTarget_code() {
+                return target_code;
+        }
+
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getDocumentation() {
+        return documentation;
+    }
+
+    public String getTerms_of_use() {
+        return terms_of_use;
+    }
+
+    public int getTime_last_update_unix() {
+        return time_last_update_unix;
+    }
+
+    public Date getTime_last_update_utc() {
+        return time_last_update_utc;
+    }
+
+    public int getTime_next_update_unix() {
+        return time_next_update_unix;
+    }
+
+    public Date getTime_next_update_utc() {
+        return time_next_update_utc;
+    }
+
+    public void setBase_code(String base_code) {
+        this.base_code = base_code;
+    }
+
+    public void setTarget_code(String target_code) {
+        this.target_code = target_code;
+    }
+
+    public double getConversion_rate() {
+        return conversion_rate;
+    }
+
+        @Override
+        public int compareTo(Moeda o) {
+                return 0;
+        }
 }
+
+
+
+
+
 
 
